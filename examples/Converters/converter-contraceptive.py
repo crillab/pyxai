4c58f9ba8267370ce51bb318ef447a11e6eb6755
nicolas.szczepanski <szczepanski.nicolas@gmail.com>
2023-01-10 10:05:22 +0100

    contraceptive
    Former-commit-id: 634cc2d3c237b090b3932bc104fa01c13fa22492

--- examples/Converters/converter-contraceptive.py
+++ examples/Converters/converter-contraceptive.py
@@ -3,8 +3,10 @@
 from pyxai import Learning, Explainer, Tools
 
 import datetime
+import pandas
+data = pandas.read_csv(Tools.Options.dataset, names=['age', 'wife-education', 'husband-education', 'children', 'religion','job', 'occupation', 'index','media', 'contraceptive_method'])
 
-converter = Learning.Converter(Tools.Options.dataset, target_feature="binaryClass", classification_type=Learning.BINARY_CLASS) # class Converter
+converter = Learning.Converter(data, target_feature="contraceptive_method", classification_type=Learning.BINARY_CLASS) # class Converter
 
 print("data:", converter.data)
 
