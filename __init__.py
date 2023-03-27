4885d3f4b9c3ab4f635d151ce8fc776599d97a17
Szczepanski Nicolas <szczepanski.nicolas@gmail.com>
2023-03-27 16:16:15 +0200

    unit test

--- __init__.py
+++ __init__.py
@@ -3,27 +3,6 @@ import sys
 from pyxai.sources.core.tools.option import Options
 from pyxai.sources.core.tools.utils import set_verbose
 
-# from pyxai.solvers.ML.scikitlearn import Scikitlearn
-# from pyxai.solvers.ML.xgboost import Xgboost
-
-# from pyxai.core.tools.utils import display_observation, Stopwatch
-
-# from pyxai.core.explainer.explainerDT import ExplainerDT
-# from pyxai.core.explainer.explainerBT import ExplainerBT
-# from pyxai.core.explainer.explainerRF import ExplainerRF
-
-# from pyxai.core.structure.type import TypeReason, TypeCount, TypeTree, EvaluationMethod, EvaluationOutput, Indexes, ReasonExpressivity
-# from pyxai.core.tools.heatmap import HeatMap
-# from pyxai.core.structure.decisionTree import DecisionTree, DecisionNode
-# from pyxai.core.structure.randomForest import RandomForest
-
-# from pyxai.core.structure.boostedTrees import BoostedTrees
-
-
-# DIRECT = TypeReason.Direct
-# SUFFICIENT = TypeReason.Sufficient
-# MINIMAL_SUFFICIENT = TypeReason.MinimalSufficient
-# ALL = TypeReason.All
 Options.set_values("dataset", "model_directory", "n_iterations", "time_limit", "verbose", "types", "output")
 Options.set_flags("f")
 Options.parse(sys.argv[1:])
