cdba0141dcb9037c18e23a580a2910badea24394
Gilles Audemard <audemard@cril.fr>
2023-03-28 18:45:01 +0200

    in progress

--- examples/regression/BT/simple.py
+++ examples/regression/BT/simple.py
@@ -1,15 +1,17 @@
 from pyxai import Learning, Explainer, Tools
 
 # Machine learning part
-learner = Learning.Xgboost(Tools.Options.dataset)
+learner = Learning.LightGBM(Tools.Options.dataset)
 model = learner.evaluate(method=Learning.HOLD_OUT, output=Learning.BT, learner_type=Learning.REGRESSION)
-instance, prediction = learner.get_instances(model=model, n=1, correct=False)
+instances= learner.get_instances(model=model, n=10)
+instance = instances[5][0]
 
 # Explanation part
 print("instance", instance)
-print("prediction", prediction)
 
 explainer = Explainer.initialize(model, instance)
+prediction = explainer.predict(instance)
+print("prediction: ", explainer.predict(instance))
 print("extremum range: ",explainer.extremum_range())
 
 direct_reason = explainer.direct_reason()
@@ -17,7 +19,7 @@ print("len direct: ", len(direct_reason))
 print("is a reason (for 50 checks):", explainer.is_reason(direct_reason))
 
 
-percent = 0.1
+percent = 0.4
 
 explainer.set_range(prediction*(1-percent), prediction*(1+percent))
 print(f"set interval to [{1-percent} * prediction, {1+percent} * prediction]: [", explainer.lower_bound, explainer.upper_bound, "]")
@@ -29,18 +31,17 @@ print("is a tree specific", explainer.is_tree_specific_reason(tree_specific_reas
 
 
 
+
+print("\n\nActivate theorie")
+explainer = Explainer.initialize(model, instance=instance, features_types={"numerical": Learning.DEFAULT})
+direct_reason = explainer.direct_reason()
+print("len direct: ", len(direct_reason))
+print("is a reason (for 50 checks):", explainer.is_reason(direct_reason))
+explainer.set_range(prediction*(1-percent), prediction*(1+percent))
+print(f"set interval to [{1-percent} * prediction, {1+percent} * prediction]: [", explainer.lower_bound, explainer.upper_bound, "]")
 #
-# print("\n\nActivate theorie")
-# explainer = Explainer.initialize(model, instance=instance, features_types={"numerical": Learning.DEFAULT})
-# direct_reason = explainer.direct_reason()
-# print("len direct: ", len(direct_reason))
-# print("is a reason (for 50 checks):", explainer.is_reason(direct_reason))
-#
-# explainer.set_range(prediction*(1-percent), prediction*(1+percent))
-# print(f"set interval to [{1-percent} * prediction, {1+percent} * prediction]: [", explainer.lower_bound, explainer.upper_bound, "]")
-#
-# tree_specific_reason = explainer.tree_specific_reason(n_iterations=1)
-# print("\ntree_specific: ", tree_specific_reason)
-# print("\nlen tree_specific: ", len(tree_specific_reason))
-# print("\ntree_specific: ", explainer.to_features(tree_specific_reason, eliminate_redundant_features=True))
-# print("is a tree specific", explainer.is_tree_specific_reason(tree_specific_reason))
+tree_specific_reason = explainer.tree_specific_reason(n_iterations=1)
+print("\ntree_specific: ", tree_specific_reason)
+print("\nlen tree_specific: ", len(tree_specific_reason))
+print("\ntree_specific: ", explainer.to_features(tree_specific_reason, eliminate_redundant_features=True))
+print("is a tree specific", explainer.is_tree_specific_reason(tree_specific_reason))
