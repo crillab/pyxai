ab2fc812c2bb60450519b123240d9379cc6c2c44
Gilles Audemard <audemard@cril.fr>
2023-06-14 17:23:50 +0200

    continue tests
    Former-commit-id: 8402c5c597c0725fcb43fe7de2200151464fbe3e

---
+++ tests/explaining/rf.py
@@ -0,0 +1,74 @@
+from pyxai import Builder, Learning, Explainer, Tools
+import unittest
+
+Tools.set_verbose(0)
+
+
+class TestRF(unittest.TestCase):
+    model = None
+
+
+    def init(cls):
+        if cls.model is None:
+            cls.learner = Learning.Scikitlearn("tests/iris.csv", learner_type=Learning.CLASSIFICATION)
+            cls.model = cls.learner.evaluate(method=Learning.HOLD_OUT, output=Learning.RF)
+        return cls.learner, cls.model
+
+
+    def setUp(self):
+        print("..|In method:", self._testMethodName)
+
+
+    def test_sufficient(self):
+        learner, model = self.init()
+        explainer = Explainer.initialize(model, features_type={"numerical": Learning.DEFAULT})
+        instances = learner.get_instances(model, n=30, correct=True)
+        for instance, prediction in instances:
+            explainer.set_instance(instance)
+            sufficient_reason = explainer.sufficient_reason(time_limit=5)
+            if explainer.elapsed_time == Explainer.TIMEOUT:
+                self.assertTrue(explainer.is_reason(sufficient_reason))
+            else:
+                self.assertTrue(explainer.is_sufficient_reason(sufficient_reason) != False)
+
+
+    def test_majoritary(self):
+        learner, model = self.init()
+        explainer = Explainer.initialize(model, features_type={"numerical": Learning.DEFAULT})
+        instances = learner.get_instances(model, n=30, correct=True)
+        for instance, prediction in instances:
+            explainer.set_instance(instance)
+            majoritary_reason = explainer.majoritary_reason()
+            self.assertTrue(explainer.is_majoritary_reason(majoritary_reason))
+
+
+    def test_minimal_majoritary(self):
+        learner, model = self.init()
+        explainer = Explainer.initialize(model, features_type={"numerical": Learning.DEFAULT})
+        instances = learner.get_instances(model, n=30, correct=True)
+        for instance, prediction in instances:
+            explainer.set_instance(instance)
+            majoritary_reason = explainer.minimal_majoritary_reason(time_limit=5)
+            self.assertTrue(len(majoritary_reason) == 0 or explainer.is_majoritary_reason(majoritary_reason))
+
+
+    def test_contrastive(self):
+        learner, model = self.init()
+        explainer = Explainer.initialize(model)
+        instances = learner.get_instances(model, n=30, correct=True)
+        for instance, prediction in instances:
+            explainer.set_instance(instance)
+            contrastive_reason = explainer.minimal_contrastive_reason(time_limit=5)
+            self.assertTrue(explainer.is_contrastive_reason(contrastive_reason))
+
+
+    def test_minimals(self):
+        learner, model = self.init()
+        explainer = Explainer.initialize(model)
+        instances = learner.get_instances(model, n=30, correct=True)
+        for instance, prediction in instances:
+            explainer.set_instance(instance)
+            minimal_reasons = explainer.minimal_sufficient_reason(n=10)
+            print("ici ", minimal_reasons)
+            for m in minimal_reasons:
+                self.assertTrue(explainer.is_sufficient_reason(m))
