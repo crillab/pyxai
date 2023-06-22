d916bbe8fe676a8d3b8bf99021f74b50d5209042
Szczepanski Nicolas <szczepanski.nicolas@gmail.com>
2023-06-22 11:25:54 +0200

    EQ and NED for binary in theory take into account.
    Former-commit-id: 40a498dfc1aa929f9663341dc5012402b47b0f8b

--- examples/DT/builder-rectify.py
+++ examples/DT/builder-rectify.py
@@ -18,12 +18,14 @@ nodeT1_2 = Builder.DecisionNode(2, left=1, right=0)
 nodeT1_1 = Builder.DecisionNode(1, left=nodeT1_2, right=nodeT1_3)
 tree = Builder.DecisionTree(3, nodeT1_1, force_features_equal_to_binaries=True)
 
+print("type:", type(tree))
 nodeT2_1 = Builder.DecisionNode(2, left=0, right=1)
 positive_rectifying_tree = Builder.DecisionTree(3, nodeT2_1, force_features_equal_to_binaries=True)
 
 nodeT3_3 = Builder.DecisionNode(3, left=0, right=1)
 nodeT3_1 = Builder.DecisionNode(1, left=1, right=nodeT3_3)
 negative_rectifying_tree = Builder.DecisionTree(3, nodeT3_1, force_features_equal_to_binaries=True)
+
 
 explainer = Explainer.initialize(tree)
 rectifying_tree = explainer.rectify(tree, positive_rectifying_tree, negative_rectifying_tree)
