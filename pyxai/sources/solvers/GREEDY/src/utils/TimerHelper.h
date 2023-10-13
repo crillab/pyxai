f3c9e6fe577a2169b145764dd9f7e6b3ba5b72c0
Szczepanski Nicolas <szczepanski.nicolas@gmail.com>
2023-10-13 10:41:29 +0200

    Windows Build

--- pyxai/sources/solvers/GREEDY/src/utils/TimerHelper.h
+++ pyxai/sources/solvers/GREEDY/src/utils/TimerHelper.h
@@ -1,59 +1,58 @@
+
 #ifndef miniMOLS_TimerHelper_h
 #define miniMOLS_TimerHelper_h
 
-namespace pyxai {
+
 
 //It is __GLIBC__ instead of __linux__ 
 #if defined(__GLIBC__)
 #include <fpu_control.h>
 #endif
 
-#if defined(_WIN32)
-// MSVC defines this in winsock2.h!?
-#include < time.h >
-#include < windows.h >
 
-#if defined(_MSC_VER) || defined(_MSC_EXTENSIONS)
-  #define DELTA_EPOCH_IN_MICROSECS  11644473600000000Ui64
-#else
-  #define DELTA_EPOCH_IN_MICROSECS  11644473600000000ULL
-#endif
-typedef struct timeval_win {
-    long tv_sec;
-    long tv_usec;
-} timeval_win;
-
-int gettimeofday_win(timeval_win * tp)
-{
-    // Note: some broken versions only have 8 trailing zero's, the correct epoch has 9 trailing zero's
-    // This magic number is the number of 100 nanosecond intervals since January 1, 1601 (UTC)
-    // until 00:00:00 January 1, 1970 
-    static const uint64_t EPOCH = ((uint64_t) 116444736000000000ULL);
-
-    SYSTEMTIME  system_time;
-    FILETIME    file_time;
-    uint64_t    time;
-
-    GetSystemTime( &system_time );
-    SystemTimeToFileTime( &system_time, &file_time );
-    time =  ((uint64_t)file_time.dwLowDateTime )      ;
-    time += ((uint64_t)file_time.dwHighDateTime) << 32;
-
-    tp->tv_sec  = (long) ((time - EPOCH) / 10000000L);
-    tp->tv_usec = (long) (system_time.wMilliseconds * 1000);
-    return 0;
-}
-#endif // _WIN32
-
-
-
-
+namespace pyxai {
     static double initRealTime = 0;
     static double initCpuTime = 0;
     static bool isInitialized = false;
 
     namespace TimerHelper {
-      
+        #if defined(_WIN32)
+        // MSVC defines this in winsock2.h!?
+        #include < time.h >
+        #include < windows.h >
+
+        #if defined(_MSC_VER) || defined(_MSC_EXTENSIONS)
+          #define DELTA_EPOCH_IN_MICROSECS  11644473600000000Ui64
+        #else
+          #define DELTA_EPOCH_IN_MICROSECS  11644473600000000ULL
+        #endif
+        typedef struct timeval_win {
+            long tv_sec;
+            long tv_usec;
+        } timeval_win;
+
+        int gettimeofday_win(timeval_win * tp)
+        {
+            // Note: some broken versions only have 8 trailing zero's, the correct epoch has 9 trailing zero's
+            // This magic number is the number of 100 nanosecond intervals since January 1, 1601 (UTC)
+            // until 00:00:00 January 1, 1970 
+            static const uint64_t EPOCH = ((uint64_t) 116444736000000000ULL);
+
+            SYSTEMTIME  system_time;
+            FILETIME    file_time;
+            uint64_t    time;
+
+            GetSystemTime( &system_time );
+            SystemTimeToFileTime( &system_time, &file_time );
+            time =  ((uint64_t)file_time.dwLowDateTime )      ;
+            time += ((uint64_t)file_time.dwHighDateTime) << 32;
+
+            tp->tv_sec  = (long) ((time - EPOCH) / 10000000L);
+            tp->tv_usec = (long) (system_time.wMilliseconds * 1000);
+            return 0;
+        }
+        #endif // _WIN32
+        
         inline double cpuTime() {
           if(!isInitialized){
             std::cout << "Warning: initializeTime() has not been called before !" << std::endl;
