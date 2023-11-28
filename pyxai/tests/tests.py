6629c667d2a0906ab1f10b146d9f7003f6dd75c3
Szczepanski Nicolas <szczepanski.nicolas@gmail.com>
2023-11-28 16:35:32 +0100

    v1.0.10

--- pyxai/tests/tests.py
+++ pyxai/tests/tests.py
@@ -24,35 +24,36 @@ from pyxai.tests.explaining.regressionbt import *
 
 def linux_tests():
     suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(TestToFeatures))
-    #suite.addTest(unittest.makeSuite(TestGetInstances))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestToFeatures))
+    suite.addTest(unittest.makeSuite(TestGetInstances))
     
-    #suite.addTest(unittest.makeSuite(TestLearningScikitlearn))
-    #suite.addTest(unittest.makeSuite(TestLearningXGBoost))
-    #suite.addTest(unittest.makeSuite(TestLearningLightGBM))
+    suite.addTest(unittest.makeSuite(TestLearningScikitlearn))
+    suite.addTest(unittest.makeSuite(TestLearningXGBoost))
+    suite.addTest(unittest.makeSuite(TestLearningLightGBM))
     
-    #suite.addTest(unittest.makeSuite(TestSaveLoadScikitlearn))
-    #suite.addTest(unittest.makeSuite(TestSaveLoadXgboost))
-    #suite.addTest(unittest.makeSuite(TestSaveLoadLightGBM))
-    #suite.addTest(unittest.makeSuite(TestImportScikitlearn))
-    #suite.addTest(unittest.makeSuite(TestImportSimpleScikitlearn))
-    #suite.addTest(unittest.makeSuite(TestImportXGBoost))
-    #suite.addTest(unittest.makeSuite(TestImportLightGBM))
-    #suite.addTest(unittest.makeSuite(TestMisc))
-    #suite.addTest(unittest.makeSuite(TestDT))
-    #suite.addTest(unittest.makeSuite(TestRF))
-    #suite.addTest(unittest.makeSuite(TestBT))
+    suite.addTest(unittest.makeSuite(TestSaveLoadScikitlearn))
+    suite.addTest(unittest.makeSuite(TestSaveLoadXgboost))
+    suite.addTest(unittest.makeSuite(TestSaveLoadLightGBM))
+    suite.addTest(unittest.makeSuite(TestImportScikitlearn))
+    suite.addTest(unittest.makeSuite(TestImportSimpleScikitlearn))
+    suite.addTest(unittest.makeSuite(TestImportXGBoost))
+    suite.addTest(unittest.makeSuite(TestImportLightGBM))
+    suite.addTest(unittest.makeSuite(TestMisc))
+    suite.addTest(unittest.makeSuite(TestDT))
+    suite.addTest(unittest.makeSuite(TestRF))
+    suite.addTest(unittest.makeSuite(TestBT))
     return suite
 
-def linux_windows():
+def windows_tests():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(TestToFeatures))
     return suite
+
 
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     runner = unittest.TextTestRunner(verbosity=2)
     if platform.system() == 'Windows':
-        runner.run(linux_windows())
+        runner.run(windows_tests())
     else:
         runner.run(linux_tests())
